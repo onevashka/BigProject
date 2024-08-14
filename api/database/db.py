e93776c0eb31f8c5fd1386b29e875280a25a1df5
fara <farid34759@mail.ru>
2024-08-14 22:07:37 +0700

    create session

---
+++ api/database/db.py
@@ -0,0 +1,23 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from config import * 
+
+
+URL = f'postgresql+asyncpg://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+
+engine = create_async_engine(URL)
+
+SessionLocal = sessionmaker(bind=engine, _class = AsyncSession)
+
+async def get_db():
+    try:
+        session = SessionLocal()
+        yield session
+    except Exception as e:
+        print(e)
+    finally:
+        await session.close()
+
+        
+
