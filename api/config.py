cf1bd8809c00c057e399934a358b61fe94aad4fb
fara <farid34759@mail.ru>
2024-08-14 21:42:41 +0700

    upgrade settings db

---
+++ api/config.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+import os 
+
+
+load_dotenv()
+
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv('DB_PORT')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_USERNAME = os.getenv('DB_USERNAME')
+DB_NAME =os.getenv('DB_NAME')
